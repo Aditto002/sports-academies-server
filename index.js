b556bbc6b3471480ce9fddfb6b0d6b2b10640da0
Aditto Koiri <adittokoiri002@gmail.com>
2023-06-10 15:00:00 +0600

    delete

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtpuxcz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,6 +59,16 @@ async function run() {
         console.log(item);
         const result = await cartsClassCollection.insertOne(item);
         res.send(result);
+    })
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: id};
+      console.log(query);
+      const result = await cartsClassCollection.deleteOne(query);
+      res.send(result);
+      
     })
 
 
