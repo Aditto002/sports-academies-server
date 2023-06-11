ebfa967a4ffdc34ebd15ed0b573b90185e40ca25
Aditto Koiri <adittokoiri002@gmail.com>
2023-06-11 22:47:47 +0600

    All user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000 ;
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 //middleware
@@ -49,6 +50,22 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
+
+   app.patch('/users/admin/:id', async(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const filter = { _id: new ObjectId(id)};
+    const updateDoc = {
+      $set: {
+        role: 'admin'
+      },
+    };
+    const result = await usersCollection.updateOne(filter,updateDoc);
+    res.send(result);
+   })
+
+
+
 
     app.get('/populer',async(req,res)=>{
         const result = await populerClassCollection.find().toArray();
